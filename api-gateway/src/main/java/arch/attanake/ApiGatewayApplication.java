7164857f889e8353c6c020765bc14cbe58927544
Attannake <valentintngfkha@mail.ru>
2025-07-16 15:50:26 +0300

    Eureka has been added Api-gateway realizing

--- api-gateway/src/main/java/arch/attanake/ApiGatewayApplication.java
+++ api-gateway/src/main/java/arch/attanake/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package arch.attanake;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ApiGatewayApplication.class).run(args);
